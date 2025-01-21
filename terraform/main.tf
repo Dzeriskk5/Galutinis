9bd80e5831c179261d9ae3bbb1b61ff583315812
Dzeriskk5 <dzeriskk@gmail.com>
2025-01-21 20:53:33 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 
     required_version = ">= 1.2.0"
     backend "s3" {
-        bucket         = "mock-tfstate-bucket"
+        bucket         = "AWSKibiriukasGalutiniam"
         key            = "terraform.tfstate"
         region         = "eu-central-1"
         encrypt        = true
